c0c7c4b81f82a6393b694a2b962a42184faf88d6
kartik Verma <kartikx04@gmail.com>
2025-02-15 18:41:09 +0530

    changed hero text

--- components/hero.tsx
+++ components/hero.tsx
@@ -6,13 +6,12 @@ export default function Hero() {
     <section className="container flex min-h-[calc(100vh-3.5rem)] max-w-screen-2xl flex-col items-center justify-center space-y-8 py-24 text-center md:py-32">
       <div className="space-y-4">
         <h1 className="bg-gradient-to-br from-foreground from-30% via-foreground/90 to-foreground/70 bg-clip-text text-4xl font-bold tracking-tight text-transparent sm:text-5xl md:text-6xl lg:text-7xl">
-          Innovate Faster with
+          haku best
           <br />
-          Amane Soft
+          vivek next
         </h1>
         <p className="mx-auto max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
-          Empowering businesses with cutting-edge software solutions. From AI-driven analytics to seamless cloud
-          integrations, we're shaping the future of technology.
+          haku with his great mind and power created an app that will be remembered in the history of cyber security.
         </p>
       </div>
       <div className="flex gap-4">
